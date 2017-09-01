f143847ddc4159fb9a66f80d1f819a7803c15c15
admin <3147358955@qq.com>
2017-09-01 16:37:42 +0800

    12

---
+++ GetHotels/GetHotels/Model/PurchaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PurchaseTableViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/9/1.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GethotelModel.h"
+@interface PurchaseTableViewController : UITableViewController
+@property (strong,nonatomic) GethotelModel *hotel;
+@end
