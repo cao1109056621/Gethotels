fd7824c3a42d69a6dc08aa11e9e52c9471545df5
admin <932237153@qq.com>
2017-09-01 10:46:24 +0800

    11

---
+++ GetHotels/GetHotels/Model/Annotation.h
@@ -0,0 +1,16 @@
+//
+//  Annotation.h
+//  GetHotels
+//
+//  Created by admin on 2017/9/1.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Annotation : NSObject <MKAnnotation>
+
+@property (nonatomic) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@end
