21157f1598b4847ea25622f0270b37abaace4aa2
admin <3147358955@qq.com>
2017-09-02 09:34:46 +0800

    11

---
+++ GetHotels/GetHotels/trteViewController.m
@@ -0,0 +1,37 @@
+//
+//  trteViewController.m
+//  GetHotels
+//
+//  Created by admin on 2017/9/2.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import "trteViewController.h"
+
+@interface trteViewController ()
+
+@end
+
+@implementation trteViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
